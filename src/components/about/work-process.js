262b3741dc7ab8fe69b446cad65cafe955a1dac5
amjadrathod@gmail.com <amjadrathod@gmail.com>
2020-06-12 13:50:23 +0530

    Creater about page

---
+++ src/components/about/work-process.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+
+const WorkProcess = () => {
+	return (
+		<section className="section py-5">
+			<div className="container">
+				<div className="row justify-content-around">
+					<div className="col-lg-12 text-center mb-2">
+						<h1 >Work Process</h1 >
+					</div>
+					<div className="col-lg-3 col-md-4 text-center hover-shadow pt-3">
+						<div className="">
+							<h4 className="mb-4">Research and Plan</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+								dolore magna aliqua.</p>
+						</div>
+					</div>
+					<div className="col-lg-3 col-md-4 text-center hover-shadow pt-3">
+						<h4 className="mb-4">Design and Develop</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua.</p>
+					</div>
+					<div className="col-lg-3 col-md-4 text-center hover-shadow pt-3">
+						<h4 className="mb-4">Deliver</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
+export default WorkProcess 
---
+++ src/pages/about.js
@@ -0,0 +1,41 @@
+import React from "react"
+import Layout from "../components/layout"
+import profile from "../images/my-profile.jpg"
+import signature from "../images/signature.png"
+import SEO from "../components/seo"
+import WorkProcess from "../components/about/work-process"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="About Me" />
+			<section className="section pt-5">
+				<div className="container">
+					<div className="row">
+						<div className="col-sm-12 pb-5">
+							<h1 className="text-center">About Me</h1>
+						</div>
+						<div className="col-md-8">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+								dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+								commodo consequat.</p>
+							<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+								Excepteur sint occaecat cupidatat non proident.</p>
+							<p>Deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+								accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+								architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia.</p>
+						</div>
+						<div className="col-md-4 text-center drag-lg-top">
+							<div className="shadow-down mb-4">
+								<img src={profile} alt="author" className="img-fluid w-100 rounded-lg border-thick border-white" />
+							</div>
+							<img src={signature} alt="signature" className="img-fluid" />
+							<h4>Amjad Rathod</h4>
+						</div>
+					</div>
+				</div>
+			</section>
+			<WorkProcess />
+  </Layout>
+)
+
+export default IndexPage
