b486b1156b44b40073debd560bb10803bf85e586
amjadrathod@gmail.com <amjadrathod@gmail.com>
2020-06-12 14:06:24 +0530

    Update file name

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import profile from "../images/my-profile.jpg"
+import profile from "../images/my-profile.JPG"
 import signature from "../images/signature.png"
 import SEO from "../components/seo"
 import WorkProcess from "../components/about/work-process"
