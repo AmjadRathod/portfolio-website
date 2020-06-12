ff2cda1c98d3d71464aaac12c96d1a21a96b1039
amjadrathod@gmail.com <amjadrathod@gmail.com>
2020-06-12 13:46:52 +0530

    Create header

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,34 @@
+import'bootstrap/dist/css/bootstrap.min.css';
+import'bootstrap/dist/js/bootstrap.bundle.min';
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header className="bg-dark" >
+    <div className="container">
+
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link className="navbar-brand" to ="/">Amjad</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to ="/about/">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to ="/contact/">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   </header>
+
 )
 
 Header.propTypes = {
