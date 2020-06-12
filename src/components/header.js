700c7a6ace7f796ea4fb7ffec4ae7120660439e9
amjadrathod@gmail.com <amjadrathod@gmail.com>
2020-06-12 14:01:31 +0530

    Remove bootstrap javascript

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,3 @@
-import'bootstrap/dist/css/bootstrap.min.css';
-import'bootstrap/dist/js/bootstrap.bundle.min';
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
